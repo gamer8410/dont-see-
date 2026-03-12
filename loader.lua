f0aee8fa90c5657388addb31d03b551f6268bacc
gamer8410 <nitinrobloxgamer8410@gmail.com>
2026-03-12 12:22:01 +0530

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,14 @@
+--[[
+
+  Scripts Developed by @Farrghii & @greensoul8410
+  Join Our Discord Server : https://discord.gg/TqJPQ7Cx9j
+
+  Script supports - 
+  Arsenal
+  Blox fruits
+  Muscle Legends (discontinued)
+  Legends of Speed
+
+]]--
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/67c5d9a82aaf4555de9fa00f044e1632.lua"))()
