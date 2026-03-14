f0c945010331bcf2aff6d411907b735758a1cba5
gamer8410 <nitinrobloxgamer8410@gmail.com>
2026-03-14 18:46:45 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,11 @@
 --[[
   Darksoul Hub 
-  Scripts Developed by @Farrghii & @greensoul8410
+  Scripts Developed by Greensoul
   Join Our Discord Server : https://discord.gg/TqJPQ7Cx9j
 
   Script supports - 
   Arsenal
   Blox fruits
-  Muscle Legends (discontinued)
   Legends of Speed
 
 ]]--
